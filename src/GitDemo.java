c732bf1187c7693cbaf5dc241255c42b46209819
Michelle Geng <michelle.x.geng@gmail.com>
2019-04-10 18:20:33 -0700

    Update GitDemo.java

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -18,6 +18,6 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		GitDemo gd = new GitDemo();
-		System.out.println(gd.equal(4, 11));
+		System.out.println(gd.equal(3, 11));
 	}
 }
